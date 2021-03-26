c6e0089ae7a56c86d445ca91bfee3e2adf1be7c7
SURAJ BISHNOI <itachi@cse.iitb.ac.in>
2021-03-26 07:53:58 +0530

    macro one arg function in .c

--- c-src/dense-numericals.c
+++ c-src/dense-numericals.c
@@ -38,6 +38,9 @@ void static inline vecf64_store(double* ptr, vecf64 v){ return vst1q_f64(ptr, v)
 
 #endif
 
+#define sleefify_f32(base_name) Sleef_##base_name##f1_u10
+#define sleefify_f64(base_name) Sleef_##base_name##d1_u10
+
 vecf32 static inline vecf32_make(float* ptr, const long stride){
   vecf32 v;
   for(int i=0; i<SIMD_SINGLE_STRIDE; i++) v[i] = ptr[i*stride];
@@ -58,109 +61,134 @@ void static inline vecf64_store_multi(vecf64 v, double* ptr, const long stride){
   for(int i=0; i< SIMD_DOUBLE_STRIDE; i++) (ptr+i*stride)[0] = v[i];
 }
 
-void DN_ssin(const long n,
-             float* x, const long ox, const long incx,
-             float* y, const long oy, const long incy){
-  x += ox, y += oy;
-  float* y_end = y + incy * n;
-  vecf32 va, vb;
-  if (incx == 1 && incy == 1){
-    float* simd_end = y + (n/SIMD_SINGLE_STRIDE)*SIMD_SINGLE_STRIDE;
-    while(y != simd_end){
-      va = vecf32_load(x);
-      vb = sleefify_vecf32(sin)(va);
-      vecf32_store(y, vb);
-      x += SIMD_SINGLE_STRIDE;
-      y += SIMD_SINGLE_STRIDE;
-    }
-  }else if(incy == 1){
-    float* simd_end = y + (n/SIMD_SINGLE_STRIDE)*SIMD_SINGLE_STRIDE;
-    while(y != simd_end){
-      va = vecf32_make(x, incx);
-      vb = sleefify_vecf32(sin)(va);
-      vecf32_store(y, vb);
-      x += SIMD_SINGLE_STRIDE*incx;
-      y += SIMD_SINGLE_STRIDE;
-    }
-  }else if(incx == 1){
-    float* simd_end = x + (n/SIMD_SINGLE_STRIDE)*SIMD_SINGLE_STRIDE;
-    while(x != simd_end){
-      va = vecf32_load(x);
-      vb = sleefify_vecf32(sin)(va);
-      vecf32_store_multi(vb, y, incy);
-      x += SIMD_SINGLE_STRIDE;
-      y += SIMD_SINGLE_STRIDE*incy;
-    }
-  }else{
-    long i=0;
-    const long simd_end = (n/SIMD_SINGLE_STRIDE)*SIMD_SINGLE_STRIDE;
-    while(i != simd_end){
-      va = vecf32_make(x, incx);
-      vb = sleefify_vecf32(sin)(va);
-      vecf32_store_multi(vb, y, incy);
-      i += SIMD_SINGLE_STRIDE;
-      x += SIMD_SINGLE_STRIDE * incx;
-      y += SIMD_SINGLE_STRIDE * incy;
-    }
-  }
-  
-  while(y!=y_end){
-    y[0] = sin(x[0]);
-    x += incx;
-    y += incy;
-  }
-};
+#define one_arg_fn_body(oaf)                                              \
+  void DN_s##oaf(const long n,                                              \
+               float* x, const long ox, const long incx,                  \
+               float* y, const long oy, const long incy){                 \
+    x += ox, y += oy;                                                     \
+    float* y_end = y + incy * n;                                          \
+    vecf32 va, vb;                                                        \
+    if (incx == 1 && incy == 1){                                          \
+      float* simd_end = y + (n/SIMD_SINGLE_STRIDE)*SIMD_SINGLE_STRIDE;    \
+      while(y != simd_end){                                               \
+        va = vecf32_load(x);                                              \
+        vb = sleefify_vecf32(oaf)(va);                                    \
+        vecf32_store(y, vb);                                              \
+        x += SIMD_SINGLE_STRIDE;                                          \
+        y += SIMD_SINGLE_STRIDE;                                          \
+      }                                                                   \
+    }else if(incy == 1){                                                  \
+      float* simd_end = y + (n/SIMD_SINGLE_STRIDE)*SIMD_SINGLE_STRIDE;    \
+      while(y != simd_end){                                               \
+        va = vecf32_make(x, incx);                                        \
+        vb = sleefify_vecf32(oaf)(va);                                    \
+        vecf32_store(y, vb);                                              \
+        x += SIMD_SINGLE_STRIDE*incx;                                     \
+        y += SIMD_SINGLE_STRIDE;                                          \
+      }                                                                   \
+    }else if(incx == 1){                                                  \
+      float* simd_end = x + (n/SIMD_SINGLE_STRIDE)*SIMD_SINGLE_STRIDE;    \
+      while(x != simd_end){                                               \
+        va = vecf32_load(x);                                              \
+        vb = sleefify_vecf32(oaf)(va);                                    \
+        vecf32_store_multi(vb, y, incy);                                  \
+        x += SIMD_SINGLE_STRIDE;                                          \
+        y += SIMD_SINGLE_STRIDE*incy;                                     \
+      }                                                                   \
+    }else{                                                                \
+      long i=0;                                                           \
+      const long simd_end = (n/SIMD_SINGLE_STRIDE)*SIMD_SINGLE_STRIDE;    \
+      while(i != simd_end){                                               \
+        va = vecf32_make(x, incx);                                        \
+        vb = sleefify_vecf32(oaf)(va);                                    \
+        vecf32_store_multi(vb, y, incy);                                  \
+        i += SIMD_SINGLE_STRIDE;                                          \
+        x += SIMD_SINGLE_STRIDE * incx;                                   \
+        y += SIMD_SINGLE_STRIDE * incy;                                   \
+      }                                                                   \
+    }                                                                     \
+                                                                          \
+    while(y!=y_end){                                                      \
+      y[0] = sleefify_f32(oaf)(x[0]);                                   \
+      x += incx;                                                          \
+      y += incy;                                                          \
+    }                                                                     \
+  };                                                                      \
+                                                                          \
+  void DN_d##oaf(const long n,                                              \
+               double* x, const long ox, const long incx,                 \
+               double* y, const long oy, const long incy){                \
+    x += ox, y += oy;                                                     \
+    double* y_end = y + incy * n;                                         \
+    vecf64 va, vb;                                                        \
+    if (incx == 1 && incy == 1){                                          \
+      double* simd_end = y + (n/SIMD_DOUBLE_STRIDE)*SIMD_DOUBLE_STRIDE;   \
+      while(y != simd_end){                                               \
+        va = vecf64_load(x);                                              \
+        vb = sleefify_vecf64(oaf)(va);                                    \
+        vecf64_store(y, vb);                                              \
+        x += SIMD_DOUBLE_STRIDE;                                          \
+        y += SIMD_DOUBLE_STRIDE;                                          \
+      }                                                                   \
+    }else if(incy == 1){                                                  \
+      double* simd_end = y + (n/SIMD_DOUBLE_STRIDE)*SIMD_DOUBLE_STRIDE;   \
+      while(y != simd_end){                                               \
+        va = vecf64_make(x, incx);                                        \
+        vb = sleefify_vecf64(oaf)(va);                                    \
+        vecf64_store(y, vb);                                              \
+        x += SIMD_DOUBLE_STRIDE*incx;                                     \
+        y += SIMD_DOUBLE_STRIDE;                                          \
+      }                                                                   \
+    }else if(incx == 1){                                                  \
+      double* simd_end = x + (n/SIMD_DOUBLE_STRIDE)*SIMD_DOUBLE_STRIDE;   \
+      while(x != simd_end){                                               \
+        va = vecf64_load(x);                                              \
+        vb = sleefify_vecf64(oaf)(va);                                    \
+        vecf64_store_multi(vb, y, incy);                                  \
+        x += SIMD_DOUBLE_STRIDE;                                          \
+        y += SIMD_DOUBLE_STRIDE*incy;                                     \
+      }                                                                   \
+    }else{                                                                \
+      long i=0;                                                           \
+      const long simd_end = (n/SIMD_DOUBLE_STRIDE)*SIMD_DOUBLE_STRIDE;    \
+      while(i != simd_end){                                               \
+        va = vecf64_make(x, incx);                                        \
+        vb = sleefify_vecf64(oaf)(va);                                    \
+        vecf64_store_multi(vb, y, incy);                                  \
+        i += SIMD_DOUBLE_STRIDE;                                          \
+        x += SIMD_DOUBLE_STRIDE * incx;                                   \
+        y += SIMD_DOUBLE_STRIDE * incy;                                   \
+      }                                                                   \
+    }                                                                     \
+                                                                          \
+    while(y!=y_end){                                                      \
+      y[0] = sleefify_f64(oaf)(x[0]);                                                   \
+      x += incx;                                                          \
+      y += incy;                                                          \
+    }                                                                     \
+  };
 
-void DN_dsin(const long n,
-             double* x, const long ox, const long incx,
-             double* y, const long oy, const long incy){
-  x += ox, y += oy;
-  double* y_end = y + incy * n;
-  vecf64 va, vb;
-  if (incx == 1 && incy == 1){
-    double* simd_end = y + (n/SIMD_DOUBLE_STRIDE)*SIMD_DOUBLE_STRIDE;
-    while(y != simd_end){
-      va = vecf64_load(x);
-      vb = sleefify_vecf64(sin)(va);
-      vecf64_store(y, vb);
-      x += SIMD_DOUBLE_STRIDE;
-      y += SIMD_DOUBLE_STRIDE;
-    }
-  }else if(incy == 1){
-    double* simd_end = y + (n/SIMD_DOUBLE_STRIDE)*SIMD_DOUBLE_STRIDE;
-    while(y != simd_end){
-      va = vecf64_make(x, incx);
-      vb = sleefify_vecf64(sin)(va);
-      vecf64_store(y, vb);
-      x += SIMD_DOUBLE_STRIDE*incx;
-      y += SIMD_DOUBLE_STRIDE;
-    }
-  }else if(incx == 1){
-    double* simd_end = x + (n/SIMD_DOUBLE_STRIDE)*SIMD_DOUBLE_STRIDE;
-    while(x != simd_end){
-      va = vecf64_load(x);
-      vb = sleefify_vecf64(sin)(va);
-      vecf64_store_multi(vb, y, incy);
-      x += SIMD_DOUBLE_STRIDE;
-      y += SIMD_DOUBLE_STRIDE*incy;
-    }
-  }else{
-    long i=0;
-    const long simd_end = (n/SIMD_DOUBLE_STRIDE)*SIMD_DOUBLE_STRIDE;
-    while(i != simd_end){
-      va = vecf64_make(x, incx);
-      vb = sleefify_vecf64(sin)(va);
-      vecf64_store_multi(vb, y, incy);
-      i += SIMD_DOUBLE_STRIDE;
-      x += SIMD_DOUBLE_STRIDE * incx;
-      y += SIMD_DOUBLE_STRIDE * incy;
-    }
-  }
-  
-  while(y!=y_end){
-    y[0] = sin(x[0]);
-    x += incx;
-    y += incy;
-  }
-};
+one_arg_fn_body(sin);
+one_arg_fn_body(cos);
+one_arg_fn_body(tan);
 
+one_arg_fn_body(asin);
+one_arg_fn_body(acos);
+one_arg_fn_body(atan);
+
+one_arg_fn_body(sinh);
+one_arg_fn_body(cosh);
+one_arg_fn_body(tanh);
+one_arg_fn_body(asinh);
+one_arg_fn_body(acosh);
+one_arg_fn_body(atanh);
+
+one_arg_fn_body(log);
+one_arg_fn_body(log10);
+one_arg_fn_body(log2);
+one_arg_fn_body(log1p);
+
+one_arg_fn_body(exp);
+one_arg_fn_body(exp2);
+one_arg_fn_body(exp10);
+one_arg_fn_body(expm1);
