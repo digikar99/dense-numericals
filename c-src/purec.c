c5e4888dfd1de322c04138b858e10d2a6b50c453
digikar99 <shubhamkarayare@gmail.com>
2021-05-05 19:57:36 +0530

    ltf1 and ltd1 interchanged were lef1 and led1

--- c-src/purec.c
+++ c-src/purec.c
@@ -3,8 +3,8 @@ float static inline Sleef_addf1_u10purec(float a, float b){return a+b;}
 float static inline Sleef_subf1_u10purec(float a, float b){return a-b;}
 float static inline Sleef_mulf1_u10purec(float a, float b){return a*b;}
 float static inline Sleef_divf1_u10purec(float a, float b){return a/b;}
-_Bool static inline Sleef_lef1_u10purec(float a, float b){return a<b;}
-_Bool static inline Sleef_ltf1_u10purec(float a, float b){return a<=b;}
+_Bool static inline Sleef_ltf1_u10purec(float a, float b){return a<b;}
+_Bool static inline Sleef_lef1_u10purec(float a, float b){return a<=b;}
 _Bool static inline Sleef_eqf1_u10purec(float a, float b){return a==b;}
 _Bool static inline Sleef_neqf1_u10purec(float a, float b){return a!=b;}
 _Bool static inline Sleef_gef1_u10purec(float a, float b){return a>=b;}
@@ -14,8 +14,8 @@ double static inline Sleef_addd1_u10purec(double a, double b){return a+b;}
 double static inline Sleef_subd1_u10purec(double a, double b){return a-b;}
 double static inline Sleef_muld1_u10purec(double a, double b){return a*b;}
 double static inline Sleef_divd1_u10purec(double a, double b){return a/b;}
-_Bool static inline Sleef_led1_u10purec(double a, double b){return a<b;}
-_Bool static inline Sleef_ltd1_u10purec(double a, double b){return a<=b;}
+_Bool static inline Sleef_ltd1_u10purec(double a, double b){return a<b;}
+_Bool static inline Sleef_led1_u10purec(double a, double b){return a<=b;}
 _Bool static inline Sleef_eqd1_u10purec(double a, double b){return a==b;}
 _Bool static inline Sleef_neqd1_u10purec(double a, double b){return a!=b;}
 _Bool static inline Sleef_ged1_u10purec(double a, double b){return a>=b;}
